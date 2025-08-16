fffda2d1af0a850b5bb6b48c31a9b475211b726d
offline <offline@localhost.localdomain>
2025-08-16 13:02:12 +0200

    Ensure correct Vite base for GH Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,8 @@
-// vite.config.js
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwind from '@tailwindcss/vite'
 
 export default defineConfig({
+  base: '/WKKOTH/',
   plugins: [react(), tailwind()],
 })
